99b181297324eae650ab512982361cf73e51f3cf
emse-p4a-gwu <john.helveston@gmail.com>
2025-11-03 10:05:58 -0500

    part 1 updated

--- render.R
+++ render.R
@@ -11,6 +11,8 @@ for (part in parts) {
     path_rmd <- file.path('parts', part, 'index.Rmd')
     path_html <- file.path('parts', part, 'index.html')
     path_pdf <- file.path('parts', part, paste0(part, '.pdf'))
+    
+    # Render
     to_html(from = path_rmd, to = path_html)
     to_pdf(from = path_html, to = path_pdf)
     # Compress the PDF to reduce size
